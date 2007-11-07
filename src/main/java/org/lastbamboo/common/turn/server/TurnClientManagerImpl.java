6e5242ef0dd13c0ee04aefc6c9eebe20150a7a3b
adamfisk <a@littleshoot.org>
2007-11-07 14:16:50 +0000

    Changed to use new EC2 utils
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/turn/server@1420 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/turn/server/TurnClientManagerImpl.java
+++ src/main/java/org/lastbamboo/common/turn/server/TurnClientManagerImpl.java
@@ -13,6 +13,7 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mina.common.IoSession;
+import org.lastbamboo.common.amazon.ec2.AmazonEc2Utils;
 import org.lastbamboo.common.util.NetworkUtils;
 
 /**
@@ -43,70 +44,9 @@ public final class TurnClientManagerImpl implements TurnClientManager
         {
         // We need to determine the public address of the EC2 server -- we need
         // to give this to clients when allocating relays.
-        m_publicAddress = getPublicAddress();
+        m_publicAddress = AmazonEc2Utils.getPublicAddress();
         }
     
-    private static InetAddress getPublicAddress()
-        {
-        // First just check if we're even on Amazon -- we could be testing
-        // locally, for example.
-        LOG.debug("Getting public address");
-        
-        // Check to see if we're running on EC2.  If we're not, we're probably 
-        // testing.  This technique could be a problem if the EC2 internal 
-        // addressing is ever different from 10.253.
-        try
-            {
-            if (!NetworkUtils.getLocalHost().getHostAddress().startsWith("10.253"))
-                {
-                // Not running on EC2.
-                LOG.debug("Not running on EC2.  Testing??");
-                return NetworkUtils.getLocalHost();
-                }
-            }
-        catch (final UnknownHostException e)
-            {
-            LOG.error("Could not get host.", e);
-            return null;
-            }
-        final String url = "http://169.254.169.254/latest/meta-data/public-ipv4";
-        final HttpClient client = new HttpClient();
-        client.getHttpConnectionManager().getParams().setConnectionTimeout(
-            10 * 1000);
-        final GetMethod method = new GetMethod(url);
-        try
-            {
-            LOG.debug("Executing method...");
-            final int statusCode = client.executeMethod(method);
-            if (statusCode != HttpStatus.SC_OK)
-                {
-                LOG.warn("ERROR ISSUING REQUEST:\n" + method.getStatusLine() + 
-                    "\n" + method.getResponseBodyAsString());
-                return null;
-                }
-            else
-                {
-                LOG.debug("Successfully wrote request...");
-                }
-            final String host = method.getResponseBodyAsString();
-            LOG.debug("Got address: "+host);
-            return InetAddress.getByName(host);
-            }
-        catch (final HttpException e)
-            {
-            LOG.error("Could not access EC2 service", e);
-            return null;
-            }
-        catch (final IOException e)
-            {
-            LOG.error("Could not access EC2 service", e);
-            return null;
-            }
-        finally 
-            {
-            method.releaseConnection();
-            }
-        }
 
     public TurnClient allocateBinding(final IoSession ioSession) 
         {
